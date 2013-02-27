66615dfee2e4118a81f99060a2ea838670bdf879
Troy Grosfield <troy.grosfield@gmail.com>
2013-02-26 23:40:38 -0600

    added template tags.

---
+++ django_social/templatetags/facebook_tags.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from django.template import Library
+from django_social.connections import FacebookConnection
+
+register = Library()
+
+@register.filter
+def fb_avatar_url(facebook_user_id=None):
+    """
+    Currency formatting template filter.
+
+    Takes a number -- integer, float, decimal -- and formats it according to
+    the locale specified as the template tag argument (arg). Examples:
+
+      * {{ value|currency }}
+      * {{ value|currency:"en_US" }}
+      * {{ value|currency:"pt_BR" }}
+      * {{ value|currency:"pt_BR.UTF8" }}
+
+    If the argument is omitted, the default system locale will be used.
+
+    The third parameter, symbol, controls whether the currency symbol will be
+    printed or not. Defaults to true.
+
+    As advised by the Django documentation, this template won't raise
+    exceptions caused by wrong types or invalid locale arguments. It will
+    return an empty string instead.
+
+    Be aware that currency formatting is not possible using the 'C' locale.
+    This function will fall back to 'en_US.UTF8' in this case.
+    """
+    if not facebook_user_id:
+        return ''
+    fb_connection = FacebookConnection(user_id=facebook_user_id, name=None)
+    return fb_connection.get_avatar_url()
---
+++ django_social/templatetags/gravatar.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Minor modificationss taken from 
+http://code.google.com/p/django-gravatar/source/browse/trunk/gravatar/templatetags/gravatar.py
+
+Examples:
+
+"""
+
+from django import template
+from django.utils.html import escape
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils.hashcompat import md5_constructor
+import urllib
+
+GRAVATAR_URL_PREFIX = getattr(settings, "GRAVATAR_URL_PREFIX", "http://www.gravatar.com/")
+GRAVATAR_DEFAULT_IMAGE = getattr(settings, "GRAVATAR_DEFAULT_IMAGE", "")
+
+register = template.Library()
+
+
+def gravatar_for_email(email, size=32):
+    url = "%savatar/%s/?" % (GRAVATAR_URL_PREFIX, md5_constructor(email).hexdigest())
+    url += urllib.urlencode({"s": str(size), "default": GRAVATAR_DEFAULT_IMAGE})
+    return escape(url)
+
+
+def gravatar_img_for_email(email, size=32):
+    url = gravatar_for_email(email, size)
+    return escape(url)
+
+
+def gravatar(user, size=32):
+    # backward compatibility
+    if hasattr(user, 'email'):
+        return gravatar_img_for_email(email=user.email, size=size)
+    # elif hasattr(user, 'email'):
+    return gravatar_img_for_email(email=user, size=size)
+
+
+register.simple_tag(gravatar)
+register.simple_tag(gravatar_for_email)
+register.simple_tag(gravatar_img_for_email)
