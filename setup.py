a528c591963582d527c94637d1409a52a2cba255
Troy Grosfield <troy.grosfield@gmail.com>
2013-11-25 07:57:30 -0700

    setup file updates

---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+classifiers = [
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+]
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(
+    name='django-social',
+    version='0.0.1',
+    description='Social app for django',
+    long_description=README,
+    author='Troy Grosfield',
+    maintainer='Troy Grosfield',
+    url='https://github.com/InfoAgeTech/django-social',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    setup_requires=[
+        'django >= 1.5.5',
+    ],
+    classifiers=classifiers
+)
