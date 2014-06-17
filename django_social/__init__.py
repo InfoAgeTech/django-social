83f8a46e389f2d171bb4d6952314af6a466db9e8
Troy Grosfield <troy.grosfield@gmail.com>
2014-06-17 11:37:09 -0600

    added youtube and instagram sources

--- django_social/__init__.py
+++ django_social/__init__.py
@@ -4,10 +4,14 @@ from __future__ import unicode_literals
 class Source(object):
     DELICIOUS = 'DELICIOUS'
     FACEBOOK = 'FACEBOOK'
+    INSTAGRAM = 'INSTAGRAM'
     PICASA = 'PICASA'
     TWITTER = 'TWITTER'
+    YOUTUBE = 'YOUTUBE'
     CHOICES = ((DELICIOUS, 'Delicious'),
                (FACEBOOK, 'Facebook'),
+               (INSTAGRAM, 'Instagram'),
                (PICASA, 'Picasa'),
-               (TWITTER, 'Twitter')
+               (TWITTER, 'Twitter'),
+               (YOUTUBE, 'YouTube')
                )
