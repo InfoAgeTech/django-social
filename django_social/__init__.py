0885952a04b2749a80baf63e9b6d52c83d604af8
Troy Grosfield <troy.grosfield@gmail.com>
2014-11-26 12:37:57 -0700

    added a way to get the nice display for a source

--- django_social/__init__.py
+++ django_social/__init__.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 class Source(object):
     DELICIOUS = 'DELICIOUS'
     FACEBOOK = 'FACEBOOK'
+    GOOGLE = 'GOOGLE'
+    GOOGLEPLUS = 'GOOGLEPLUS'
     INSTAGRAM = 'INSTAGRAM'
     PICASA = 'PICASA'
     TWITTER = 'TWITTER'
@@ -11,9 +13,23 @@ class Source(object):
     SOUNDCLOUD = 'SOUNDCLOUD'
     CHOICES = ((DELICIOUS, 'Delicious'),
                (FACEBOOK, 'Facebook'),
+               (GOOGLE, 'Google'),
+               (GOOGLEPLUS, 'Google+'),
                (INSTAGRAM, 'Instagram'),
                (PICASA, 'Picasa'),
                (SOUNDCLOUD, 'SoundCloud'),
                (TWITTER, 'Twitter'),
                (YOUTUBE, 'YouTube')
                )
+
+    @classmethod
+    def get_display(cls, source):
+        """Gets the display value for a source."""
+        if not source:
+            return
+
+        for k, display in cls.CHOICES:
+            if k == source:
+                return display
+
+        return source
